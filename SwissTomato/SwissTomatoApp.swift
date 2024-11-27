a9e045cacb96ec8148b7c10f9a4a9314e62c72f4
Chen, Zian (USA) <Zian.Chen2@rtx.com>
2024-11-26 19:24:22 -0500

    Initial Commit

---
+++ SwissTomato/SwissTomatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwissTomatoApp.swift
+//  SwissTomato
+//
+//  Created by Zian Chen on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SwissTomatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
